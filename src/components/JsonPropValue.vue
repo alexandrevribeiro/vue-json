e3850210ae9ab74eb90a54c9d7c1067d694536aa
alexandrevribeiro <vribeiro.alexandre@gmail.com>
2017-09-12 01:03:04 -0300

    Rendering recursive JSON object (not completly working yet)

--- src/components/JsonPropValue.vue
+++ src/components/JsonPropValue.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="json-value inline">
         <!-- Dynamically loads the right component -->
-        <component :is="valueComponentName"></component>
+        <component :is="valueComponentName" v-bind="valueComponentProps"></component>
     </div>
 </template>
 
@@ -13,22 +13,25 @@
     import JsonValueString from './JsonValueString.vue'
     import JsonValueNumber from './JsonValueNumber.vue'
     import JsonValueBoolean from './JsonValueBoolean.vue'
+    import JsonObject from './JsonObject.vue'
 
     @Component({
-        props: { 
-            propMetadata: JsonPropMetadata 
-        },
         components: {
             JsonValueString,
             JsonValueNumber,
-            JsonValueBoolean
+            JsonValueBoolean,
+            JsonObject
         }
     })
     export default class Json extends Vue {
         
-        @Prop()
+        @Prop({required: true})
         propMetadata: JsonPropMetadata
 
+        /**
+         * Gets the name of the component to be rendered from
+         * the "propMetadata.type".
+         */
         get valueComponentName() {
             
             switch (this.propMetadata.type) {
@@ -42,11 +45,25 @@
                     return 'json-value-boolean';
 
                 case JsonPropTypeEnum.Object:
+                    return 'json-object';
+
+                case JsonPropTypeEnum.Array:
                     throw new Error('Not implemented!');
             
                 default:
                     return 'json-value-string';
             }
+        }
+
+        /**
+         * Gets the dynamic props to be passed to the current dynamic 
+         * component (when there are props).
+         */
+        get valueComponentProps() {
+            if (this.propMetadata.type == JsonPropTypeEnum.Object) 
+                return { 
+                    jsonObjMetadata: this.propMetadata.objectProps 
+                };
         }
      }
 </script>
